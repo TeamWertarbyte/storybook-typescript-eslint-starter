3e7206725b12225866638d6aed5306c0e2f33e33
Sascha Becker <s.becker@wertarbyte.com>
2020-07-31 15:09:48 +0200

    Add viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../stories/**/*.stories.js', '../src/components/*.stories.(js|mdx)'],
   addons: [
     '@storybook/addon-docs',
+    '@storybook/addon-viewport/register',
     '@storybook/addon-knobs/register',
     '@storybook/addon-actions',
     '@storybook/addon-links',
